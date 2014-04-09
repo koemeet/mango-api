99d741c8eb1f454ae3f04384533e89cb83a6b379
robinjansen <robinjansen51@gmail.com>
2014-04-09 12:28:58 +0000

    1) User model updated (http://pastebin.com/XL88aXbW fields added) 2) User model config file updated
    (.)(.)

--- src/Mango/CoreDomain/Model/User.php
+++ src/Mango/CoreDomain/Model/User.php
@@ -26,6 +26,47 @@ class User extends BaseUser
      */
     protected $applications;
 
+    protected $firstName;
+
+    protected $inbetweener;
+
+    protected $middleName;
+
+    protected $lastName;
+
+    protected $phonePrimair;
+
+    protected $phoneSecundair;
+
+    protected $streetAddress;
+
+    protected $streetNumber;
+
+    protected $streetNumberSuffix;
+
+    protected $city;
+
+    protected $zipcode;
+
+    protected $country;
+
+    protected $billingStreetAddress;
+
+    protected $billingStreetNumber;
+
+    protected $billingStreetNumberSuffix;
+
+    protected $billingCity;
+
+    protected $billingZipcode;
+
+    protected $billingCountry;
+
+    protected $marketingMails;
+
+    protected $notes;
+
+
     /**
      * @param Customer $customer
      */
@@ -79,4 +120,326 @@ class User extends BaseUser
     {
         return $this->applications;
     }
+
+    /**
+     * @param mixed $billingCity
+     */
+    public function setBillingCity($billingCity)
+    {
+        $this->billingCity = $billingCity;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingCity()
+    {
+        return $this->billingCity;
+    }
+
+    /**
+     * @param mixed $billingCountry
+     */
+    public function setBillingCountry($billingCountry)
+    {
+        $this->billingCountry = $billingCountry;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingCountry()
+    {
+        return $this->billingCountry;
+    }
+
+    /**
+     * @param mixed $billingStreetAddress
+     */
+    public function setBillingStreetAddress($billingStreetAddress)
+    {
+        $this->billingStreetAddress = $billingStreetAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingStreetAddress()
+    {
+        return $this->billingStreetAddress;
+    }
+
+    /**
+     * @param mixed $billingStreetNumber
+     */
+    public function setBillingStreetNumber($billingStreetNumber)
+    {
+        $this->billingStreetNumber = $billingStreetNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingStreetNumber()
+    {
+        return $this->billingStreetNumber;
+    }
+
+    /**
+     * @param mixed $billingStreetNumberSuffix
+     */
+    public function setBillingStreetNumberSuffix($billingStreetNumberSuffix)
+    {
+        $this->billingStreetNumberSuffix = $billingStreetNumberSuffix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingStreetNumberSuffix()
+    {
+        return $this->billingStreetNumberSuffix;
+    }
+
+    /**
+     * @param mixed $billingZipcode
+     */
+    public function setBillingZipcode($billingZipcode)
+    {
+        $this->billingZipcode = $billingZipcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBillingZipcode()
+    {
+        return $this->billingZipcode;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $inbetweener
+     */
+    public function setInbetweener($inbetweener)
+    {
+        $this->inbetweener = $inbetweener;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInbetweener()
+    {
+        return $this->inbetweener;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $marketingMails
+     */
+    public function setMarketingMails($marketingMails)
+    {
+        $this->marketingMails = $marketingMails;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMarketingMails()
+    {
+        return $this->marketingMails;
+    }
+
+    /**
+     * @param mixed $middleName
+     */
+    public function setMiddleName($middleName)
+    {
+        $this->middleName = $middleName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMiddleName()
+    {
+        return $this->middleName;
+    }
+
+    /**
+     * @param mixed $notes
+     */
+    public function setNotes($notes)
+    {
+        $this->notes = $notes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * @param mixed $phonePrimair
+     */
+    public function setPhonePrimair($phonePrimair)
+    {
+        $this->phonePrimair = $phonePrimair;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhonePrimair()
+    {
+        return $this->phonePrimair;
+    }
+
+    /**
+     * @param mixed $phoneSecundair
+     */
+    public function setPhoneSecundair($phoneSecundair)
+    {
+        $this->phoneSecundair = $phoneSecundair;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoneSecundair()
+    {
+        return $this->phoneSecundair;
+    }
+
+    /**
+     * @param mixed $streetAddress
+     */
+    public function setStreetAddress($streetAddress)
+    {
+        $this->streetAddress = $streetAddress;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreetAddress()
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * @param mixed $streetNumber
+     */
+    public function setStreetNumber($streetNumber)
+    {
+        $this->streetNumber = $streetNumber;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreetNumber()
+    {
+        return $this->streetNumber;
+    }
+
+    /**
+     * @param mixed $streetNumberSuffix
+     */
+    public function setStreetNumberSuffix($streetNumberSuffix)
+    {
+        $this->streetNumberSuffix = $streetNumberSuffix;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreetNumberSuffix()
+    {
+        return $this->streetNumberSuffix;
+    }
+
+    /**
+     * @param mixed $zipcode
+     */
+    public function setZipcode($zipcode)
+    {
+        $this->zipcode = $zipcode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipcode()
+    {
+        return $this->zipcode;
+    }
+
+
 }
