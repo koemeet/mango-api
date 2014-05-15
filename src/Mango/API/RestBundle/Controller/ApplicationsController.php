ab3e01cbb0b3a9d3fdffacb1788cbd1c2f1b4896
steffenbrem <steffenbrem@gmail.com>
2014-05-15 14:22:54 +0000

    Form errors are now arrays instead of strings.

--- src/Mango/API/RestBundle/Controller/ApplicationsController.php
+++ src/Mango/API/RestBundle/Controller/ApplicationsController.php
@@ -25,11 +25,15 @@ use Mango\CoreDomain\Model\Application;
 use Mango\CoreDomain\Model\User;
 use Mango\CoreDomain\Repository\ApplicationRepositoryInterface;
 use Mango\CoreDomain\Repository\CategoryRepositoryInterface;
+use Mango\CoreDomain\Repository\MenuRepositoryInterface;
 use Mango\CoreDomain\Repository\PageRepositoryInterface;
 use Mango\CoreDomainBundle\Form\ApplicationType;
 use Mango\CoreDomainBundle\Form\UserType;
 use Mango\CoreDomainBundle\Service\PageService;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use PHPCR\Util\NodeHelper;
+use Symfony\Cmf\Bundle\MenuBundle\Doctrine\Phpcr\Menu;
+use Symfony\Cmf\Bundle\MenuBundle\Doctrine\Phpcr\MenuNode;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
@@ -85,6 +89,40 @@ class ApplicationsController extends RestController
      */
     public function getApplicationsAction(ParamFetcherInterface $paramFetcher)
     {
+        /** @var DocumentManager $dm */
+        $dm = $this->get('doctrine_phpcr.odm.document_manager');
+//
+//        /** @var MenuRepositoryInterface $repo */
+//        $repo = $this->get('mango_core_domain.menu_repository');
+//        $menu = $repo->createMenu();
+
+        $parentPath = '/applications/01cd0360-d11c-11e3-89b3-a8b38a9a6428/menus';
+        $session = $dm->getPhpcrSession();
+        NodeHelper::createPath($session, $parentPath);
+        $parentNode = $dm->find(null, $parentPath);
+
+        $menu = new Menu();
+        $menu->setName('tann2');
+        $menu->setUri('http://www.example.com');
+        $menu->setLabel('My menu!!');
+        $menu->setParent($parentNode);
+
+        $dm->persist($menu);
+
+        $node = new MenuNode();
+        $node->setName('home');
+        $node->setLabel('Homepage!');
+        $node->setUri('http://www.google.nl');
+        $node->setParent($menu);
+
+        $dm->persist($node);
+
+        $menu->addChild($node);
+
+        $dm->flush();
+
+        die;
+
         $query = $this->queryExtractor->extract($paramFetcher);
         $applications = $this->applicationRepository->findByQuery($query)->getArrayCopy();
 
